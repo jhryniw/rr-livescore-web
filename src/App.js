93419b9fbcd91879d19246cf573d147cd3b47aa6
James Hryniw <jhryniw@gmail.com>
2017-12-31 15:14:31 -0700

    Complete scoresheet ui (#6)
    * minor changes
    
    * segmented scoresheet page
    
    * complete scoresheet ui

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Livescore from './components/livescore';
-import ScoreSheet from './components/scoresheet';
+import ScoreSheet from './components/scoresheet/scoresheet';
 import './App.css';
 
 const App = () => (
     <Router>
         <div className="App">
             <Route exact path="/" component={Livescore} />
-            <Route path="/scoresheet" component={ScoreSheet} />
+            <Route path="/scoresheet/:match" component={ScoreSheet} />
         </div>
     </Router>
 );
